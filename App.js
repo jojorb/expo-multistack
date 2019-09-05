b98f714ba829a3dc6e7196fbbc9e5d787c540635
Remi Roubin <robyremzy@disroot.org>
2019-09-05 23:31:11 +0200

    add apollo

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { AppContainer } from './app/navigation/SwitchNav';
+import { GRAPHQLBACKEND, GRAPHQLTOKEN } from './config';
 
-const App = () => {
-  console.log('app started');
-  return <AppContainer />;
-};
+const clientgraphql = new ApolloClient({
+  uri: GRAPHQLBACKEND,
+  request: async operation => {
+    // operation.setContext({
+    //   fetchOptions: {
+    //     credentials: 'include',
+    //   },
+    //   headers,
+    // });
+    const token = GRAPHQLTOKEN;
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+});
+
+const App = () => (
+  // console.log('app home screen')
+  <ApolloProvider client={clientgraphql}>
+    <AppContainer />
+  </ApolloProvider>
+);
 export default App;
---
+++ config.js
@@ -0,0 +1,2 @@
+export const GRAPHQLBACKEND = '';
+export const GRAPHQLTOKEN = '';
